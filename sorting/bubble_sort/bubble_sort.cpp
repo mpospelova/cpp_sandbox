5110d7bb99376691f043369df9e1fb678d3f1f70
Masha <masha@mashahost.localdomain>
2020-04-07 13:00:40 +0200

    bubble sort

---
+++ sorting/bubble_sort/bubble_sort.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+
+// using namespace std;
+
+void countSwaps(std::vector<int> a) {
+    int n = a.size();
+    int swaps = 0;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n - 1; j++) {
+            if (a[j] > a[j + 1]) {
+                std::swap(a[j], a[j + 1]);
+                swaps++;
+            }
+        }
+    }
+
+    std::cout << "Array is sorted in " << swaps << " swaps." << std::endl;
+    std::cout << "First Element: " << a[0] << std::endl;
+    std::cout << "Last Element: " << a[a.size()-1] << std::endl;
+}
+
+
+int main() {
+    std::vector<int> vec = {3, 2, 1};
+    countSwaps(vec);
+    return 0;
+}
